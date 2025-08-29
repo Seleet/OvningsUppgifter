984469968ebd24e15bb656b516dde33e62fed5b3
Martin Selin <seleeto@gmail.com>
2025-08-29 09:42:29 +0200

    Första commit - la till alla övningar

---
+++ IFSatser/3Termometer/3Termometer/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vilken temp visar termometern?");
+double temp = Convert.ToDouble(Console.ReadLine());
+if (temp > 37.8 && temp <= 39.5)
+{
+    Console.WriteLine("Du har feber");
+}
+else if (temp > 39.5)
+{
+    Console.WriteLine("Du bör uppsöka läkare");
+}
+else
+{
+    Console.WriteLine("Du har inte feber.");
+}
