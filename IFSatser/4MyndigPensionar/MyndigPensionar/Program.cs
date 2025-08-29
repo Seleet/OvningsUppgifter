4466baf508ad9b357171cd19ae6da68d425aa63b
Martin Selin <seleeto@gmail.com>
2025-08-29 10:17:23 +0200

    Skapat programmet

---
+++ IFSatser/4MyndigPensionar/MyndigPensionar/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hej, vad är din ålder?");
+string? userAge = Console.ReadLine();
+
+if (int.TryParse(userAge, out int age))
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Du är inte myndig.");
+    }
+    else if (age >= 65)
+    {
+        Console.WriteLine($"Du är {age} år och pensionär.");
+    }
+    else
+    {
+        Console.WriteLine($"Du är {age} år och myndig men inte pensionär.");
+    }
+}
+else
+{
+    Console.WriteLine("Ogiltig ålder angiven, försök igen.");
+}
