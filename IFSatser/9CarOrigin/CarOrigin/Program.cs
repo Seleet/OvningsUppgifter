32916dde3140b786d27a1bde5e18a6e14e432aa5
Martin Selin <seleeto@gmail.com>
2025-08-30 15:44:19 +0200

    Bilmärkesconsoleapp

---
+++ IFSatser/9CarOrigin/CarOrigin/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Mata in ett bilmärke: ");
+string carBrand =  Console.ReadLine().ToLower();
+if (carBrand == "volvo")
+{
+    
+}
+else if (carBrand == "volkswagen" || carBrand == "bmw" || carBrand == "audi")
+{
+    Console.WriteLine("Bilen är tysk.");
+}
+else if (carBrand == "renault" || carBrand == "peugeot" || carBrand == "citroen")
+{
+    Console.WriteLine("Bilen är fransk.");
+}
+else 
+{
+}
+
