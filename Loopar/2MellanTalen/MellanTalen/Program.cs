e81788ce6eba87f847c65c38469ee02225199459
Martin Selin <seleeto@gmail.com>
2025-08-30 16:35:37 +0200

    mellantalen

---
+++ Loopar/2MellanTalen/MellanTalen/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Skriv ett heltal:");
+int tal1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Skriv ett annat heltal:");
+int tal2 = Convert.ToInt32(Console.ReadLine());
+
+if (tal1 < tal2)
+{
+    for (int i = tal1; i <= tal2; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+else if (tal1 > tal2)
+{
+    for (int i = tal1; i >= tal2; i--)
+    {
+        Console.WriteLine(i);
+    }
+}
+else
+{
+    Console.WriteLine("Talen är lika: " + tal1);
+}
