d08cc60a4d075a497754be4e7c22bd7ad3de0c81
Martin Selin <seleeto@gmail.com>
2025-08-30 16:52:52 +0200

    två tal, beräkningar

---
+++ Variabler/9TwoNumbers/TwoNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Mata in ett tal:");
+int input = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Mata in ett annat tal:");
+int input2 = int.Parse(Console.ReadLine());
+
+int sum = input + input2;
+int average = sum / 2;
+int difference = Math.Abs(input - input2);
+
+Console.WriteLine($"Medelvärdet: {average}\nSumman: {sum}\nDifferensen: {difference}");
